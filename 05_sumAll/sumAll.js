ab725a2771f983fd7d5150fa637407ba23121c99
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-08-09 22:01:31 +0100

    Updated `sumAll` function
    returned and sum up all the positive integers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (minNum, maxNum) {
+  let sumNum = 0;
+  if (minNum > maxNum) {
+    let temp = minNum;
+    minNum = maxNum;
+    maxNum = temp;
+  }
+  for (minNum; minNum <= maxNum; minNum++) {
+    sumNum += minNum;
+  }
 
+  if (sumNum < 0) {
+    return "ERROR";
+  } else if (Number.isInteger(minNum) && Number.isInteger(maxNum)) {
+    return sumNum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
