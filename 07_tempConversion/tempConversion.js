445c3b3a3ca57374707058d7ceb7150cbea08ddc
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-08-11 22:06:30 +0100

    Updated covertToCelsius and convertToFahrenheit
    converted celcius to fahrenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fh) {
+  let result = (fh - 32) * (5 / 9);
+  if (!Number.isInteger(result)) {
+    return +result.toFixed(1);
+  } else {
+    return result;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  let result = celcius * (9 / 5) + 32;
+  if (!Number.isInteger(result)) {
+    return +result.toFixed(1);
+  } else {
+    return result;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
