52b1b7e37f6849073d07fb065b2ed5c43f69ca76
fidelisobioma <mbamfidelisobioma1@gmail.com>
2024-09-13 11:23:30 +0100

    Ekcercises solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let currentDate = new Date().getFullYear();
+  return people.reduce((oldest, current) => {
+    let oldAge = oldest.yearOfDeath
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : currentDate - oldest.yearOfBirth;
+    let currentAge = oldest.yearOfDeath
+      ? current.yearOfDeath - current.yearOfBirth
+      : currentDate - current.yearOfBirth;
+    return currentAge > oldAge ? current : oldest;
+  }, people[0]);
 };
 
 // Do not edit below this line
